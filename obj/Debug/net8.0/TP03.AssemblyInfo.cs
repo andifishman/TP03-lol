a2b56b591a01244809afac3737c639a3cc4457ed
VICTORIA JOANA KRIGER CONTI <49439065@ort>
2025-05-08 15:40:12 -0300

    terminado final

--- obj/Debug/net8.0/TP03.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77dfb0217173ebce9c3e4701632b9b86665991f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f693d9e393c3ee967a854e1be0f4bceeb4e308b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
